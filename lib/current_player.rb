ede25e6b144a8cc28cff972d8efcc70c4aa5f535
Sanima Shrestha <saneemastha@gmail.com>
2019-06-29 14:44:10 +0000

    Automatically backed up by Learn

---
+++ lib/current_player.rb
@@ -0,0 +1,14 @@
+def turn_count
+  if position?board[]==""
+    count+=1 
+    current_player(count)
+  end
+end
+
+def current_player(count)
+  if count%2==0 
+    reutrn "x"
+  else
+    return "o"
+  
+end
